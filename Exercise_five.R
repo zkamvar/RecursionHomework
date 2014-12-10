6178b01418b94c406694780c9ccf1f35eefa2dde
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2014-12-10 11:03:41 -0800

    Add exercise_five

---
+++ Exercise_five.R
@@ -0,0 +1,11 @@
+#' Global Alignment
+#' ================
+#' 
+#' ### Packages
+library(stringr)
+library(rstackdeque)
+library(hash)
+source("Exercise_four.R")
+devtools::session_info()
+ls()
+#' 
